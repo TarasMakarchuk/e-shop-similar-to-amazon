54698d3f7dd5788703ad1fd88f31c5e6a78e81a7
Taras Makarchuk <2278452@gmail.com>
2023-10-13 22:01:12 +0300

    :zap: add faker-jw, passport, jwt, argon2, jwt strategy, jwt config, validate auth dto

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post('register')
-  async register() {
-    return this.authService.register();
+  async register(@Body() dto: AuthDto) {
+    return this.authService.register(dto);
   }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,11 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { PrismaService } from '../prisma.service';
+import { faker } from '@faker-js/faker';
+import { hash } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
 
-  register() {
-    return {
-      name: 'User'
-    }
+  async register(dto: AuthDto) {
+    const {email} = dto;
+
+    const existUser = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (existUser) throw new BadRequestException('User is already exists');
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: faker.internet.userName(),
+        avatarPath: faker.image.avatar(),
+        phone: faker.helpers.replaceSymbolWithNumber('+3 (###) ###-##-##'),
+        password: await hash(dto.password),
+      },
+    });
+
+    return user;
+  }
+
+  private async issueTokens(userId: number) {
+    const data = {id: userId};
+
+    const accessToken = this.jwt.sign(data, {
+      expiresIn: '1h',
+    });
+
+    const refreshToken = this.jwt.sign(data, {
+      expiresIn: '7d',
+    });
+
+    return { accessToken, refreshToken };
   }
 }

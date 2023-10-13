baa38fa1bfe4d2f1dd0da4e2cd84bb14af0aeb23
Taras Makarchuk <2278452@gmail.com>
2023-10-13 16:23:29 +0300

    initialize server side

---
+++ server/src/main.ts
@@ -0,0 +1,17 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const logger = new Logger('Main');
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const HOST = process.env.HOST || 'localhost';
+  const PORT = process.env.PORT || 5001;
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  await app.listen(PORT);
+  logger.log(`Server is running on http://${HOST}:${PORT}/api`);
+}
+bootstrap();

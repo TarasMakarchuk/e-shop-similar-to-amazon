9d78f51ddb561303883e897dd464df6147e1a927
Taras Makarchuk <2278452@gmail.com>
2023-10-14 20:16:09 +0300

    :zap: add user methods, update schema

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { CurrentUser } from '../auth/decorators/user.decorator';
+import { UserDto } from './dto/user.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('profile')
+  @Auth()
+  async getProfile(@CurrentUser('id') id: number) {
+    return this.userService.byId(id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put('profile/update')
+  @Auth()
+  async update(@CurrentUser('id') id: number, @Body() dto: UserDto) {
+    return this.userService.updateProfile(id, dto);
+  }
+
+  @Auth()
+  @Patch('profile/favorites/:productId')
+  async toggleFavorite(
+    @CurrentUser('id') id: number,
+    @Param('productId') productId: string,
+  ) {
+    return this.userService.toggleFavoriteProduct(id, +productId);
+  }
 }

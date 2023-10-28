62dd23440363e043156eb7a7934719c7af0a2c8e
Taras Makarchuk <2278452@gmail.com>
2023-10-28 19:23:11 +0300

    add review

--- server/src/review/review.controller.ts
+++ server/src/review/review.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ReviewService } from './review.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+import { ReviewDto } from './dto/review.dto';
 
-@Controller('review')
+@Controller('reviews')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+	constructor(private readonly reviewService: ReviewService) {}
+
+	@UsePipes(new ValidationPipe())
+	@Get()
+	async getAll() {
+		return this.reviewService.getAll();
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Auth()
+	@Post('leave/:productId')
+	async leaveReview(
+		@CurrentUser('id') id: number,
+		@Body() dto: ReviewDto,
+		@Param('productId') productId: number,
+	) {
+		return this.reviewService.create(id, dto, +productId);
+	}
 }

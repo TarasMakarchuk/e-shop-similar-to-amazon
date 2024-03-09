25d14eba6e5b8b00988974a612cd7f6794d46062
Taras Makarchuk <2278452@gmail.com>
2024-03-09 18:18:10 +0200

    update main.ts file

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,13 +6,14 @@ import { PrismaService } from './prisma.service';
 const logger = new Logger('Main');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const HOST = process.env.HOST || 'localhost';
-  const PORT = process.env.PORT || 5001;
+	const app = await NestFactory.create(AppModule);
+	const HOST = process.env.HOST || 'localhost';
+	const PORT = process.env.PORT || 5001;
 
-  app.setGlobalPrefix('api');
-  app.enableCors();
-  await app.listen(PORT);
-  logger.log(`Server is running on http://${HOST}:${PORT}/api`);
+	app.setGlobalPrefix('api');
+	app.enableCors();
+	await app.listen(PORT);
+	logger.log(`Server is running on http://${HOST}:${PORT}/api`);
 }
+
 bootstrap();

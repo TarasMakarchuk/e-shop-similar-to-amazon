b4bac4b035f4c898bfe01a1a2688c1c649ba145b
Taras Makarchuk <2278452@gmail.com>
2024-09-19 20:27:05 +0300

    update user.service.ts

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -73,6 +73,7 @@ export class UserService {
 		if (!user) {
 			throw new NotFoundException('User not found');
 		}
+		
 		const isProductExists = user.favorites.some(
 			product => product.id === productId,
 		);

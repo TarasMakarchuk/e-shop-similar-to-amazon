7b613172e6919a948b6c8573890b233b3d6df68f
Taras Makarchuk <2278452@gmail.com>
2024-10-23 20:03:34 +0300

    update auth.module.ts

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { PrismaService } from '../prisma.service';
-import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { getJwtConfig } from '../config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
+
 import { JwtStrategy } from './strategy/jwt.strategy';
+import { getJwtConfig } from '../config/jwt.config';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService, PrismaService, JwtStrategy],
-  imports: [
-    ConfigModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: getJwtConfig,
-    }),
-  ],
+	controllers: [AuthController],
+	providers: [AuthService, PrismaService, JwtStrategy],
+	imports: [
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig,
+		}),
+	],
 })
-  
 export class AuthModule {}
